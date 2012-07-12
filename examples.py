ac5761e852af8b43d647c7586c5a1235cca74434
oskarbi <oscar.teixe@gmail.com>
2012-07-12 23:48:35 +0200

    Examples rewritten.

--- examples.py
+++ examples.py
@@ -8,52 +8,48 @@ import querify
 
 def run_select_examples():
     """SELECT statement generating example."""
-    table = "customers"
-    select_fields = ['name', 'last_name', 'age', 'country']
-    select_conds = {}
-    select_conds2 = {'id': 150}
-    select_conds3 = {'id': 150, 'nombre': "oscar"}
+    table = "actors"
+    select_fields = ['name', 'last_name', 'country']
+    select_conds1 = {}
+    select_conds2 = {'id': 3}
+    select_conds3 = {'id': 3, 'name': "Matt"}
     print querify.select_from_dict(table, select_fields)
-    print querify.select_from_dict(table, select_fields, select_conds)
+    print querify.select_from_dict(table, select_fields, select_conds1)
     print querify.select_from_dict(table, select_fields, select_conds2)
     print querify.select_from_dict(table, select_fields, select_conds3)
 
 def run_insert_example():
     """INSERT statement generating example."""
-    table = "customers"
+    table = "actors"
     insert_values = {
-        'id': 150,
-        'name': "Oscar",
-        'last_name': "Teixeira",
-        'age': 42,
-        'country': "Spain"}
+        'id': 3,
+        'name': "Matt",
+        'last_name': "Smith",
+        'country': "England"}
     print querify.insert_from_dict(table, insert_values)
 
-    insert_col_list = ["id", "name", "last_name", "age", "country"]
+    insert_col_list = ["id", "name", "last_name", "country"]
     insert_val_list = [
-        [150, "Oscar", "Teixeira", 42, "Spain"],
-        [150, "Oscar", "Teixeira", 42, "Spain"],
-        [150, "Oscar", "Teixeira", 42, "Spain"]
-        ]
+        [1, "Chris", "Eccleston", "England"],
+        [2, "David", "Tennant", "Scotland"],
+        [3, "Matt", "Smith", "England"]]
     print querify.insert_from_list(table, insert_col_list, insert_val_list)
 
 def run_update_example():
     """UPDATE statement generating example."""
-    table = "customers"
+    table = "actors"
     update_values = {
-        'name': "Oscar",
-        'last_name': "Teixeira Martin",
-        'age': 43,
-        'country': "Italy"}
-    update_conds = {'id': 150}
+        'name': "Christopher",
+        'last_name': "Eccleston"}
+    update_conds = {'id': 1}
     print querify.update_from_dict(table, update_values, update_conds)
 
 def run_delete_example():
     """DELETE statement generating example."""
-    table = "customers"
+    table = "actors"
     delete_conds = {
-        'name': "Oscar",
-        'last_name': "Teixeira"}
+        'name': "Matt",
+        'last_name': "Smith"}
     print querify.delete_from_dict(table, delete_conds)
 
 
